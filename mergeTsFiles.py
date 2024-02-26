1cece85dd8ccd2d1b75a8551284c0bbcc4f32d0d
dopaminehigh <tranhvu72@gmail.com>
2024-02-26 11:58:55 -0500

    Revert "Update merging and download example"
    This reverts commit 2ba0a3fab8243d2b62a9a097b5a343acd14530d0.

--- mergeTsFiles.py
+++ mergeTsFiles.py
@@ -1,24 +1,17 @@
 import os
-import re
 import subprocess
 
-def natural_sort_key(s):
-    # Split the filename into non-digit and digit parts
-    parts = re.split(r'(\d+)', s)
-    # Convert the digit parts to integers and keep the non-digit parts as strings
-    return [int(part) if part.isdigit() else part for part in parts]
-
 def merge_ts_files(input_dir, output_file, ffmpeg_path) -> None:
-    # List all .ts files in the input directory and sort them
-    ts_files = sorted([file for file in os.listdir(input_dir) if file.endswith('.ts')], key=natural_sort_key)
+    # List all .ts files in the input directory
+    ts_files = [file for file in os.listdir(input_dir) if file.endswith('.ts')]
 
     # Generate a text file listing all .ts files
     with open('input.txt', 'w') as f:
-        for ts_file in ts_files:
+        for ts_file in sorted(ts_files):
             f.write(f"file '{os.path.join(input_dir, ts_file)}'\n")
 
     # Run ffmpeg command to merge the .ts files
-    command = ['ffmpeg', '-y', '-f', 'concat', '-safe', '0', '-i', 'input.txt','-vsync', '1', '-r', '30', '-c:v', 'libx265', '-crf', '28', '-preset', 'medium', '-c:a', 'copy', output_file]
+    command = [os.path.join(ffmpeg_path, 'ffmpeg'), '-y', '-f', 'concat', '-safe', '0', '-i', 'input.txt', '-c', 'copy', output_file]
     subprocess.run(command)
 
     # Clean up temporary files
@@ -27,8 +20,8 @@ def merge_ts_files(input_dir, output_file, ffmpeg_path) -> None:
 if __name__ == "__main__":
     count = 1
     input_dir = "downloaded_ts_files/"
-    output_file = "merged_video*.mp4"
-    ffmpeg_path = "" 
+    output_file = "merged_video.mp4"
+    ffmpeg_path = "ffmpeg-6.1-amd64-static/"
 
     while os.path.exists(input_dir + "%d" % count):
         input_dir_merge = input_dir + "%d" % count
